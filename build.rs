9d7427a774c3d3af06195ea52dceec0e38630ae2
Daichi Aoki <blz.soft@outlook.jp>
2022-07-17 01:14:40 +0900

    add 出力するexeのメタデータを設定した

---
+++ build.rs
@@ -0,0 +1,13 @@
+extern crate winapi;
+extern crate winres;
+
+fn main() {
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_language(winapi::um::winnt::MAKELANGID(
+            winapi::um::winnt::LANG_JAPANESE, 
+             winapi::um::winnt::SUBLANG_JAPANESE_JAPAN));
+        res.compile().unwrap();
+    }
+  }
+  
