63320efc62d590145adff9dc21784a191e7ac987
Daichi Aoki <blz.soft@outlook.jp>
2022-07-17 02:34:34 +0900

    add Edge Driverのバージョン番号確認を行い、必要に応じでダウンロードするようにした

---
+++ README.md
@@ -0,0 +1,14 @@
+# dl_edge_driver
+
+Tool to download Microsoft Edge drivers.  
+This application works only on windows.  
+This application may no longer work due to changes in the Edge specifications.  
+
+When double-clicked or executed on the command line, the edge driver will be downloaded to the same location as this application's exe. If the file already exists, check the version number and download it if necessary.
+
+Microsoft Edge Driver ダウンロードツールです。  
+このアプリケーションはWindowsでの動作します。  
+Micorsoft Edgeの仕様変更によりこのアプリケーションは動作しなくなる可能性があります。  
+
+ダブルクリックまたはコマンドラインで実行すると、このアプリケーションのexeと同じ場所にEdge Driverがダウンロードされます。  
+すでにファイルがある場合はバージョン番号を確認して、必要であればダウンロードを行います。
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use anyhow::{Result, Context};
+use anyhow::{Result, Context, Ok};
 
 
 fn main() -> Result<()> {
-    println!("edge driverのダウンロードを開始します");
-    let driver_path = fetch_edge_driver::save_driver_with_exe()?;
-    println!("edge driverのダウンロードを完了しました");
-    println!("edge driverファイルパス: {}",
-        driver_path.to_str().context("edge driverパスの文字列化に失敗した")?);
+    println!("Edge Driverの確認・ダウンロードを行います");
+    let (is_fetch, driver_path) = fetch_edge_driver::check_get_driver_with_exe()?;
+    match is_fetch {
+        fetch_edge_driver::FetchNew::Yes => {
+            println!("Edge Driverのダウンロードを完了しました");
+            println!("Edge Driverファイルパス: {}",
+            driver_path.to_str().context("Edge Driverパスの文字列化に失敗した")?);
+        },
+        _ => {
+            println!("Edge Driverは最新でした");
+            println!("Edge Driverファイルパス: {}",
+            driver_path.to_str().context("Edge Driverパスの文字列化に失敗した")?);
+        }
+    }
     Ok(())
 }
