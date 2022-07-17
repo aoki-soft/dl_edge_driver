67e4f570251b96d71c0a0c7dd4bc1d55389a6dc7
aoki-soft <aoki.soft@outlook.jp>
2022-07-17 23:32:21 +0900

    add iconを追加した

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ extern crate winres;
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico");
         res.set_language(winapi::um::winnt::MAKELANGID(
             winapi::um::winnt::LANG_JAPANESE, 
              winapi::um::winnt::SUBLANG_JAPANESE_JAPAN));
